3d95e7e84e5f4208210f5047f7a9fefa2d98f8db
batistein <sven.batista-steinbach@syself.com>
2022-10-28 16:51:18 +0200

    :seedling: adding argocd

---
+++ .chezmoitemplates/after/bin_scripts/ensure-argocd.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+check_argocd_installed() {
+  # If argocd is not available on the path, get it
+  if ! [ -x "$(command -v argocd)" ]; then
+    echo 'argocd not found, installing'
+    install_argocd
+  fi
+}
+
+
+verify_argocd_version() {
+
+  local argocd_version
+  argocd_version="$(argocd version --client --short | awk '{print $2}' | grep -Eo "([0-9]{1,}\.)+[0-9]{1,}")"
+  if [[ "${MINIMUM_ARGOCD_VERSION}" != $(echo -e "${MINIMUM_ARGOCD_VERSION}\n${argocd_version}" | sort -s -t. -k 1,1n -k 2,2n -k 3,3n | head -n1) ]]; then
+    cat <<EOF
+Detected argocd version: v${argocd_version}.
+Requires v${MINIMUM_ARGOCD_VERSION} or greater.
+Please install v${MINIMUM_ARGOCD_VERSION} or later.
+
+EOF
+    
+    confirm "$@" && echo 'Installing Clusterctl' && install_argocd
+  else
+    cat <<EOF
+Detected argocd version: v${argocd_version}.
+Requires v${MINIMUM_ARGOCD_VERSION} or greater.
+Nothing to do!
+
+EOF
+  fi
+}
+
+confirm() {
+    # call with a prompt string or use a default
+    echo "${1:-Do you want to install? [y/N]}"
+    read -r -p "" response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            return 2
+            ;;
+    esac
+}
+
+install_argocd() {
+    if [[ "${OSTYPE}" == "linux"* ]]; then
+      curl -sLo "argocd" https://github.com/argoproj/argo-cd/releases/download/v${MINIMUM_ARGOCD_VERSION}/argocd-linux-amd64
+      copy_binary
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      curl -sLo "argocd" https://github.com/argoproj/argo-cd/releases/download/v${MINIMUM_ARGOCD_VERSION}/argocd-darwin-amd64
+      copy_binary
+    else
+      set +x
+      echo "The installer does not work for your platform: $OSTYPE"
+      exit 1
+    fi
+
+}
+
+function copy_binary() {
+  if [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
+      if [ ! -d "$HOME/.local/bin" ]; then
+        mkdir -p "$HOME/.local/bin"
+      fi
+      mv argocd "$HOME/.local/bin/argocd"
+      chmod +x "$HOME/.local/bin/argocd"
+  else
+      echo "Installing Clusterctl to /usr/local/bin which is write protected"
+      echo "If you'd prefer to install Clusterctl without sudo permissions, add \$HOME/.local/bin to your \$PATH and rerun the installer"
+      sudo mv argocd /usr/local/bin/argocd
+      chmod +x "/usr/local/bin/argocd"
+  fi
+  echo "Installation Finished"
+}
+
+check_argocd_installed "$@"
+verify_argocd_version "$@"
